09a6a4fafb5ec2d7c8973d0c7b81705faee02b60
Rostom Baccar <81230614+rostom-baccar@users.noreply.github.com>
2022-04-28 16:13:50 +0200

    Updated data script (example)

--- Data-Extraction/data-script.sh
+++ Data-Extraction/data-script.sh
@@ -1,15 +1,14 @@
-#Creatigng output file
+  GNU nano 4.8                                            script.sh                                                      
+#Creating output file
 rm raw-data
 touch raw-data
-
-cd all-fzn
-#Looping over all instances
-for fzn in *; do 
-
-	./mistral-fz -t 10 --details --nb_configurations 10 data/example_satisfaction.fzn
-	sleep 11;
-	echo $(./mistral-fz -t 10 --nb_configurations 5 data/example_satisfaction.fzn)
-	>> /home/rostom/Desktop/Projects/PIR---Improving-Solvers/Data-Extraction/raw-data;
-	
+cd data/some-fzn/ecp
+for fzn in *; do
+        cd ../../..;
+        #echo $(./mistral-fz -t 5 --nb_configurations 5 all-fzn/$fzn)>> /minizinc/Mistral-PIR/fz/raw-data;
+        #cd all-fzn;
+        echo $(./mistral-fz -t 5 --nb_configurations 5 data/some-fzn/ecp/$fzn) >> /minizinc/Mistral-PIR/fz/raw-data;
+        cd data/some-fzn/ecp;
 done
+
 
